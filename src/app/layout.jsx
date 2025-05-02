4d85c7a122aad5d88c71d43559704f13492ad8f8
Kornel-crypto <novakoka66@gmail.com>
2025-05-02 11:12:42 +0200

    FRO-9 - Ajout du footer simple et responsive

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,8 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} bg-background text-foreground`}>
         <Navbar />
         <main className="p-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
 }
-
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-background text-foreground text-center py-4 mt-10 border-t border-gray-300 dark:border-gray-700">
+      <p className="text-sm">
+        © {new Date().getFullYear()} FitCoach. Tous droits réservés.
+      </p>
+    </footer>
+  );
+}
