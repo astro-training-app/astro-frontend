a741ba306b4b05d347792bb541e800ca5eef08f8
Kornel-crypto <novakoka66@gmail.com>
2025-05-11 14:27:50 +0200

    feat(GET) road recovery get measurement

--- src/app/voir-mensurations/page.jsx
+++ src/app/voir-mensurations/page.jsx
@@ -1,15 +1,13 @@
 "use client";
-import FormulaireMensurations from "@/app/mensurations/FormulaireMensurations";
 import ListeMensurations from "@/components/mensurations/ListeMensurations";
 
-
-export default function PageMensurations() {
+export default function PageVoirMensurations() {
   return (
     <main className="min-h-screen bg-gray-900 text-white p-10">
       <h1 className="text-3xl font-bold mb-6 text-center">
-        Ajouter une mensuration
+        📋 Mensurations enregistrées
       </h1>
-      <FormulaireMensurations />
+      <ListeMensurations />
     </main>
   );
 }
