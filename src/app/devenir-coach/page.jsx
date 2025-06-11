5642a6c23fa2c87c2aa949fc1e40a2aafb188bf5
Arthur <arthursimonin88@gmail.com>
2025-06-11 13:46:04 +0200

    feat: Update register endpoint

--- src/app/devenir-coach/page.jsx
+++ src/app/devenir-coach/page.jsx
@@ -5,10 +5,14 @@ import { toast } from "react-hot-toast";
 import MotionLayoutWrapper from "@/components/MotionLayoutWrapper";
 import { useRooter } from "next/navigation";
 
-const url = "http://localhost:3000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const REGISTER_ENDPOINT = process.env.NEXT_PUBLIC_REGISTER_ENDPOINT;
+
+const REGISTER_URL = `${API_URL}${REGISTER_ENDPOINT}`;
 
 export default function DevenirCoach() {
-  const [nom, setNom] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
@@ -20,10 +24,10 @@ export default function DevenirCoach() {
 
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
-    const body = JSON.stringify({ nom, email, password });
+    const body = JSON.stringify({ name, email, password });
 
     try {
-      const res = await fetch(url + "/auth/register", {
+      const res = await fetch(REGISTER_URL, {
         method: "POST",
         headers,
         body,
@@ -57,8 +61,8 @@ export default function DevenirCoach() {
             <label className="block mb-1 text-sm font-bold">Nom</label>
             <input
               type="text"
-              value={nom}
-              onChange={(e) => setNom(e.target.value)}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="w-full border border-subtitle px-3 py-2 rounded focus:outline-primary"
               placeholder="John Doe"
               required
